9be885cfcb9d1c5c9662050d229a506f04a3635f
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-06-07 23:38:17 +0530

    project added

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# restaurant-website
+# My-first-web-Project
+
+Hello Guys,
+<br>
+Visit - https://saksham-raghuvanshi.github.io/My-first-web-Project/
+<br>
+
+Follow My Instagram pages: @frontendgyaan
+<br>
+Follow me on Linkedin: saksham-raghuvanshi
+
---
+++ resources/JS/myscript.js
@@ -0,0 +1,26 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+
+ $(document).ready(function(){
+  $("a").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
